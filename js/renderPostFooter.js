2f7304d21bed85aa316701578025941df5ac07c6
LaurynasAnd <67257508+LaurynasAnd@users.noreply.github.com>
2020-07-23 10:27:06 +0300

    basic style

--- js/renderPostFooter.js
+++ js/renderPostFooter.js
@@ -1,5 +1,8 @@
 function renderPostFooter (data) {
-    return `<div class="past-footer">FOOTER</div>`;
+    return `<div class="past-footer">
+                <div class="row">LIKE COMMENT</div>
+                <div class="row">COMMENT</div>
+            </div>`;
 }
 
 export default renderPostFooter;
